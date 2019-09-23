3e580b769e95fb1f6820a016b199c7493cef6eb1
chbbs231 <atrobles231@gmail.com>
2019-09-22 19:35:30 -0700

    questions fixed

--- assets/java/app.js
+++ assets/java/app.js
@@ -1,36 +1,41 @@
-/*const questions= [{
+let questions= [{
  q1 = `Which movie features Bruce Willis as John McClane, a New York police officer, taking on a gang of criminals in a Los Angeles skyscraper on Christmas Eve?`,
-  choices= `A.) Die hard
-            B.) The Whole Nine Yards
-            C.) Red
-            D.) Tears of the Sun`
-          },{
+  choiceA: `Die hard`,
+  choiceB:  `The Whole Nine Yards`,
+  choiceC:  `Red`,
+  choiceD:  `Tears of the Sun`,
+  correct: `A`
+     },{
   q2=`What is the name of the hobbit played by Elijah Wood in the Lord of the Rings movies?`,
-  choices= `A.) ryan Newman
-            B.) Frodo
-            C.) John M. Brinnin
-            D.) Mumble`
+  choiceA: `Ryan Newman`,
+  choiceB: `Frodo`,
+  choiceC: `John M. Brinnin`,
+  choiceD: `Mumble`,
+  correct: `B`
           },{
   q3 =`Which 1997 science fiction movie starring Will Smith and Tommy Lee Jones tells the story of a secret agency that polices alien refugees who are living on earth disguised as humans?`,
-  choices= `A.) Wild Wild West
-            B.) MIB
-            C.) Independence day
-            D.) Hancock`
+  choiceA: `Wild Wild West`,
+  choiceB: `MIB`,
+  choiceC: `Independence day`,
+  choiceD: `Hancock`,
+  correct: `B`
           },{
   q4 = `Which Tom Hanks movie won the Academy Award for Best Picture in 1994?`,
-  choices = `A.) Toy Story
-             B.) Forest Gump
-             C.) Saving Private Ryan
-             D.) Vault of Horrors 1` 
+  choiceA: `Toy Story`,
+  choiceB: `Forest Gump`,
+  choiceC: `Saving Private Ryan`,
+  choiceD: `Vault of Horrors 1`,
+  correct: `B`
 },{
   q5 = ` Who played Jack Dawson in the 1997 epic Titanic?`,
-  choices = `A.) Keanu Reeves
-             B.) Tom Hanks
-             C.) Ryan Reynolds
-             D.) Leonardo DiCaprio
-`}
+  choiceA: `Keanu Reeves`,
+  choiceB: `Tom Hanks`,
+  choiceC:  `Ryan Reynolds`,
+  choiceD: `Leonardo DiCaprio`,
+  correct: `D`
+}
 ]
-  */
+
   
 M.Modal.init(document.querySelectorAll('.modal'),{})
 
