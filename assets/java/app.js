1faf77dc846b26eee626b5ba62840ce74bfe8389
chbbs231 <atrobles231@gmail.com>
2019-09-22 23:35:58 -0700

    renders something but not question

--- assets/java/app.js
+++ assets/java/app.js
@@ -1,33 +1,42 @@
-let questions= [{
- q1 = `Which movie features Bruce Willis as John McClane, a New York police officer, taking on a gang of criminals in a Los Angeles skyscraper on Christmas Eve?`,
+
+const question = document.getElementById(`quiz`)
+const choiceA = document.getElementById(`optionA`)
+const choiceB = document.getElementById(`optionB`)
+const choiceC = document.getElementById(`optionC`)
+const choiceD = document.getElementById(`optionD`)
+const count = document.getElementById(`timer`)
+const progress = document.getElementById( `score`)
+
+let tQuestions= [{
+  question1:`Which movie features Bruce Willis as John McClane, a New York police officer, taking on a gang of criminals in a Los Angeles skyscraper on Christmas Eve?`,
   choiceA: `Die hard`,
   choiceB:  `The Whole Nine Yards`,
   choiceC:  `Red`,
   choiceD:  `Tears of the Sun`,
   correct: `A`
      },{
-  q2=`What is the name of the hobbit played by Elijah Wood in the Lord of the Rings movies?`,
+  question2:`What is the name of the hobbit played by Elijah Wood in the Lord of the Rings movies?`,
   choiceA: `Ryan Newman`,
   choiceB: `Frodo`,
   choiceC: `John M. Brinnin`,
   choiceD: `Mumble`,
   correct: `B`
           },{
-  q3 =`Which 1997 science fiction movie starring Will Smith and Tommy Lee Jones tells the story of a secret agency that polices alien refugees who are living on earth disguised as humans?`,
+  question3: `Which 1997 science fiction movie starring Will Smith and Tommy Lee Jones tells the story of a secret agency that polices alien refugees who are living on earth disguised as humans?`,
   choiceA: `Wild Wild West`,
   choiceB: `MIB`,
   choiceC: `Independence day`,
   choiceD: `Hancock`,
   correct: `B`
           },{
-  q4 = `Which Tom Hanks movie won the Academy Award for Best Picture in 1994?`,
+  question4: `Which Tom Hanks movie won the Academy Award for Best Picture in 1994?`,
   choiceA: `Toy Story`,
   choiceB: `Forest Gump`,
   choiceC: `Saving Private Ryan`,
   choiceD: `Vault of Horrors 1`,
   correct: `B`
 },{
-  q5 = ` Who played Jack Dawson in the 1997 epic Titanic?`,
+  question5: ` Who played Jack Dawson in the 1997 epic Titanic?`,
   choiceA: `Keanu Reeves`,
   choiceB: `Tom Hanks`,
   choiceC:  `Ryan Reynolds`,
@@ -39,11 +48,22 @@ let questions= [{
   
 M.Modal.init(document.querySelectorAll('.modal'),{})
 
+
+
 const starttrv = () =>{
-  M.Modal.getInstance(document.getElementById('game')).open()
+  const lastQuestion = tQuestions.length -1;
+  
+  let renderQuestion = (`tQuestions`)
+  question.innerHTML = `${`tQuestion`}`;
+  optionA.innerHTML = `${`choiceA`}`;
+  optionB.innerHTML = `${`choiceB`}`;
+  optionC.innerHTML = `${`choiceC`}`;
+  optionD.innerHTML = `${`choiceD`}`;
+  M.Modal.getInstance(document.getElementById('game')).open();
 }
 const next = () =>{}
-
 document.getElementById('starttrv').addEventListener('click', starttrv)
-  
 document.getElementById('nextquest').addEventListener('click', nextquest)
+
+
+
