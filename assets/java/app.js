13717fb40b8214ad7cbade7c60fee56e52f9f161
chbbs231 <atrobles231@gmail.com>
2019-09-23 23:46:40 -0700

    renders one question

--- assets/java/app.js
+++ assets/java/app.js
@@ -1,69 +1,90 @@
+let counter =15;
+let currentQuest =2;
+let score =0;
+let timer;
 
 const question = document.getElementById(`quiz`)
-const choiceA = document.getElementById(`optionA`)
-const choiceB = document.getElementById(`optionB`)
-const choiceC = document.getElementById(`optionC`)
-const choiceD = document.getElementById(`optionD`)
 const count = document.getElementById(`timer`)
 const progress = document.getElementById( `score`)
 
-let tQuestions= [{
-  question1:`Which movie features Bruce Willis as John McClane, a New York police officer, taking on a gang of criminals in a Los Angeles skyscraper on Christmas Eve?`,
-  choiceA: `Die hard`,
-  choiceB:  `The Whole Nine Yards`,
-  choiceC:  `Red`,
-  choiceD:  `Tears of the Sun`,
-  correct: `A`
-     },{
-  question2:`What is the name of the hobbit played by Elijah Wood in the Lord of the Rings movies?`,
-  choiceA: `Ryan Newman`,
-  choiceB: `Frodo`,
-  choiceC: `John M. Brinnin`,
-  choiceD: `Mumble`,
-  correct: `B`
-          },{
-  question3: `Which 1997 science fiction movie starring Will Smith and Tommy Lee Jones tells the story of a secret agency that polices alien refugees who are living on earth disguised as humans?`,
-  choiceA: `Wild Wild West`,
-  choiceB: `MIB`,
-  choiceC: `Independence day`,
-  choiceD: `Hancock`,
-  correct: `B`
-          },{
-  question4: `Which Tom Hanks movie won the Academy Award for Best Picture in 1994?`,
-  choiceA: `Toy Story`,
-  choiceB: `Forest Gump`,
-  choiceC: `Saving Private Ryan`,
-  choiceD: `Vault of Horrors 1`,
-  correct: `B`
+const tQuestions= [
+  {
+  question:`Which movie features Bruce Willis as John McClane, a New York police officer, taking on a gang of criminals in a Los Angeles skyscraper on Christmas Eve?`,
+  choices: [ `Die hard`,
+            `The Whole Nine Yards`,
+            `Red`,
+            `Tears of the Sun`,],
+  correctans: `Die Hard`
+     },
+     {
+  question:`What is the name of the hobbit played by Elijah Wood in the Lord of the Rings movies?`,
+  choices: [  `Ryan Newman`,
+              `Frodo`,
+              `John M. Brinnin`,
+              `Mumble`,],
+  correctans: `Frodo`
+          },
+          {
+  question: `Which 1997 science fiction movie starring Will Smith and Tommy Lee Jones tells the story of a secret agency that polices alien refugees who are living on earth disguised as humans?`,
+  choices: [ `Wild Wild West`,
+              `MIB`,
+              `Independence day`,
+              `Hancock`,],
+  correctans: `MIB`
+          },
+          {
+  question: `Which Tom Hanks movie won the Academy Award for Best Picture in 1994?`,
+  choices: [ `Toy Story`,
+             `Forest Gump`,
+             `Saving Private Ryan`,
+             `Vault of Horrors 1`,],
+  correctans: `Forest Gump`
 },{
-  question5: ` Who played Jack Dawson in the 1997 epic Titanic?`,
-  choiceA: `Keanu Reeves`,
-  choiceB: `Tom Hanks`,
-  choiceC:  `Ryan Reynolds`,
-  choiceD: `Leonardo DiCaprio`,
-  correct: `D`
+  question: ` Who played Jack Dawson in the 1997 epic Titanic?`,
+  choices: [ `Keanu Reeves`,
+             `Tom Hanks`,
+             `Ryan Reynolds`,
+             `Leonardo DiCaprio`,],
+   correctans:`Leonardo DiCaprio`
 }
 ]
+function renderQuestion(){
+   const question  = tQuestions[currentQuest].question;
+   const choices = tQuestions[currentQuest].choices;
 
   
-M.Modal.init(document.querySelectorAll('.modal'),{})
-
-
-
-const starttrv = () =>{
-  const lastQuestion = tQuestions.length -1;
-  
-  let renderQuestion = (`tQuestions`)
-  question.innerHTML = `${`tQuestion`}`;
-  optionA.innerHTML = `${`choiceA`}`;
-  optionB.innerHTML = `${`choiceB`}`;
-  optionC.innerHTML = `${`choiceC`}`;
-  optionD.innerHTML = `${`choiceD`}`;
-  M.Modal.getInstance(document.getElementById('game')).open();
+  quiz.innerHTML = (`<h1>`+ question +`</h1>`);
+  choices.innerHTML = (`<h2>`+ choices +`</h2>`);
 }
-const next = () =>{}
+  
+renderQuestion();
+
+function loadchoices (choices){
+  let result = ``;
+  for (let i = 0; i < choices.length; i++){
+    results += `<p class = "choices" data-answers="${choices[i]}">${choices[i]}</P>`;
+  }
+  return result;
+}
+M.Modal.init(document.querySelectorAll('.modal'),{})
+M.Modal.getInstance(document.getElementById('game',)).open();
 document.getElementById('starttrv').addEventListener('click', starttrv)
-document.getElementById('nextquest').addEventListener('click', nextquest)
+document.getElementById('nextquest').addEventListener('click', nextquest).open();
 
 
+  
+
+
+  
+ 
+    
+  
+  
+
+
+
+
+
+const next = () =>{
+const nextquest = tQuestions.length -1;}
 
